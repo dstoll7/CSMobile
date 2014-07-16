6e0a91a659d81386b83e04a1cf53c131ef0620b2
Daniel Stoll <dstoll@andrew.cmu.edu>
2014-07-16 17:04:29 -0400

    Added the Login View Controller

---
+++ CSMobile/CSMobile/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  CSMobile
+//
+//  Created by Daniel Stoll on 7/16/14.
+//  Copyright (c) 2014 Daniel Stoll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
